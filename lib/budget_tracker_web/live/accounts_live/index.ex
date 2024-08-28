d1e0853ebf4cd615319c2b7fa9b13db2dd3f0d7d
dmitry-sidorov <jimmaggot@mail.ru>
2024-08-28 16:41:33 +0300

    Make income and payment modals work

--- lib/budget_tracker_web/live/accounts_live/index.ex
+++ lib/budget_tracker_web/live/accounts_live/index.ex
@@ -3,19 +3,27 @@ defmodule BudgetTrackerWeb.DebitAccountsLive do
 
   embed_templates "components/*"
 
+  @new_income_modal_name "new-income-modal"
+  @new_payment_modal_name "new-payment-modal"
+
   attr :amount, :float, required: true
   attr :currency, :string, required: true
   attr :title, :string, required: true
+  attr :debit_account_id, :integer, default: 999
+  attr :new_income_modal_name, :string, default: @new_income_modal_name
+  attr :new_payment_modal_name, :string, default: @new_payment_modal_name
   def account_card(assigns)
 
-  def handle_event("add_income", _unsigned_params, socket) do
+  def handle_event("add_income", unsigned_params, socket) do
     IO.puts("Add income event!")
-    socket = assign(socket, show_new_income_modal: true) |> dbg()
+    unsigned_params |> dbg()
+    socket = assign(socket, debit_account_id: unsigned_params["payload"])
     {:noreply, socket}
   end
 
-  def handle_event("add_payment", _unsigned_params, socket) do
+  def handle_event("add_payment", unsigned_params, socket) do
     IO.puts("Add payment event!")
+    socket = assign(socket, debit_account_id: unsigned_params["payload"])
     {:noreply, socket}
   end
 
@@ -25,21 +33,26 @@ defmodule BudgetTrackerWeb.DebitAccountsLive do
     {:noreply, socket}
   end
 
-  @spec render(any()) :: Phoenix.LiveView.Rendered.t()
   def render(assigns) do
-    assigns.show_new_income_modal |> dbg()
-
     ~H"""
     <div class="flex flex-col items-center gap-5">
       <.link patch={~p"/debit_accounts/new"}>
         <.button phx-click="add_debit_account">Add new debit account</.button>
       </.link>
       <div class="mx-auto max-w-sm flex flex-col items-center gap-5">
-        <.account_card amount={50.0} currency="USD" title="Запас" />
-        <.account_card amount={500.0} currency="BYN" title="Текущий аккаунт" />
+        <.account_card amount={50.0} currency="USD" title="Запас" debit_account_id={42} />
+        <.account_card
+          amount={500.0}
+          currency="BYN"
+          title="Текущий аккаунт"
+          debit_account_id={999}
+        />
       </div>
-      <.modal id="debit-account-modal" show={@show}>
-        <div>Add new payment</div>
+      <.modal id={@new_income_modal_name}>
+        <div>Add new income id: (<%= @debit_account_id %>)</div>
+      </.modal>
+      <.modal id={@new_payment_modal_name}>
+        <div>Add new payment id: (<%= @debit_account_id %>)</div>
       </.modal>
     </div>
     """
@@ -49,6 +62,9 @@ defmodule BudgetTrackerWeb.DebitAccountsLive do
     socket =
       assign(socket, show_new_payment_modal: false)
       |> assign(show_new_income_modal: false)
+      |> assign(debit_account_id: nil)
+      |> assign(new_income_modal_name: @new_income_modal_name)
+      |> assign(new_payment_modal_name: @new_payment_modal_name)
       |> assign(show: false)
 
     {:ok, socket}
