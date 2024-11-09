c8c318ddaef20ba06afd9a4a098061f2d6f7a39e
dmitry-sidorov <jimmaggot@mail.ru>
2024-11-10 00:57:32 +0300

    Add enum for operation category

--- priv/repo/migrations/20240526194719_create_operation_categories.exs
+++ priv/repo/migrations/20240526194719_create_operation_categories.exs
@@ -1,11 +1,14 @@
 defmodule BudgetTracker.Repo.Migrations.CreateOperationCategories do
   use Ecto.Migration
+  import EctoEnumMigration
 
   def change do
+    create_type(:operation_category_purpose, [:primary, :secondary])
+
     create table(:operation_categories, primary_key: false) do
       add :id, :binary_id, primary_key: true
       add :title, :string
-      add :is_primary, :boolean, default: false, null: false
+      add :purpose, :operation_category_purpose
 
       timestamps(type: :utc_datetime)
     end
